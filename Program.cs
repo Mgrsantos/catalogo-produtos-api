9bcc71d10f257255b708ee9d2db8e8c32aae4cb4
Gabriela Rondon <111393536+Mgrsantos@users.noreply.github.com>
2026-04-28 16:13:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using CatalogoProdutos.API.Data;
+using CatalogoProdutos.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=catalogo.db"));
+
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
